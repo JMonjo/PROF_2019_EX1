125d6e7f3c2e069a83863a2ca686d5ba11f48710
JAIME JOSE MONJO RODRIGUEZ <x150372@ccfi.fi.upm.es>
2019-10-29 16:13:37 +0100

    Jaime Monjo - Examen finalizado

--- src/main/java/es/upm/grise/profundizacion2019/ex1/ClaseExamen.java
+++ src/main/java/es/upm/grise/profundizacion2019/ex1/ClaseExamen.java
@@ -3,20 +3,23 @@ package es.upm.grise.profundizacion2019.ex1;
 public class ClaseExamen {
 	
 	public float metodoAProbar(int a, int b) {
-		
+		//nodo 1	
 		float result = 0;
 		
-		while( a > b) {
+		while( a > b/*nodo2*/) {
+			//nodo3
 			a--;
 		}
 		
-		if (a <= b) {
+		if (a <= b/*nodo4*/) {
+			//nodo5
 			result = 0;
 		}
+		//nodo6
 		else {
 			result = b;
 		}
-		
+		//nodo 7
 		return result;
 	}
 
--- src/test/java/es/upm/grise/profundizacion2019/ex1/ClaseExamenTest.java
+++ src/test/java/es/upm/grise/profundizacion2019/ex1/ClaseExamenTest.java
@@ -3,12 +3,29 @@ package es.upm.grise.profundizacion2019.ex1;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.Before;
 
 public class ClaseExamenTest {
 
+	private ClaseExamen test;
+
+	@Before
+	public void before(){
+		test = new ClaseExamen();
+	}
+
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test1() {
+		int a = 1; int b = 2;
+		float exp = 0;
+		assertEquals(exp, test.metodoAProbar(a,b),0);
+	}
+
+	@Test
+	public void test2() {
+		int a = 5; int b = 1;
+		float exp = 0;
+		assertEquals(exp, test.metodoAProbar(a,b),0);
 	}
 
 }
